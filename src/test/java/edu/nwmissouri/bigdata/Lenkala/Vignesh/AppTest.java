ceaffac170e15d48e68d1cdfe29cd143c8386b2c
vigneshwar6666 <vigneshwar6666@gmail.com>
2021-02-19 14:08:35 -0600

    Added files

---
+++ src/test/java/edu/nwmissouri/bigdata/Lenkala/Vignesh/AppTest.java
@@ -0,0 +1,20 @@
+package edu.nwmissouri.bigdata.Lenkala.Vignesh;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
